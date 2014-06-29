fac905f73e3fadae199540123cc41fcc9c2ad83d
Stefan `Sec` Zehl <sec@42.org>
2014-06-29 17:35:35 +0200

    Initial checkin of stuff

---
+++ cut-and-downmix-2.py
@@ -0,0 +1,141 @@
+import struct
+import sys
+import math
+import numpy
+import os.path
+import cmath
+import filters
+import scipy.signal
+
+from itertools import izip
+import matplotlib.pyplot as plt
+
+def grouped(iterable, n):
+    "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
+    return izip(*[iter(iterable)]*n)
+
+file_name = sys.argv[1]
+sample_rate = 2000000
+symbols_per_second = 25000
+preamble_length = 64
+fft_length = int(math.pow(2, int(math.log(sample_rate/symbols_per_second*preamble_length,2))))
+#fft_length = int(sample_rate/symbols_per_second*preamble_length*0.85)
+#fft_length = int(sample_rate/symbols_per_second*preamble_length*1)
+fft_step = fft_length / 50
+skip = fft_length / 20
+skip = 0
+
+print 'fft_length', fft_length
+struct_len = 8
+
+def normalize(v):
+    m = max(v)
+
+    return [x/m for x in v]
+
+def signal_start(signal):
+    max_fft = []
+    l = []
+    for i in range(0, len(signal), fft_step):
+        slice = signal[i:i+fft_length]
+        #fft_result = numpy.fft.fft(slice * numpy.blackman(len(slice)))
+        fft_result = numpy.fft.fft(slice)
+        max_mag = max([abs(x) for x in fft_result])
+        max_fft.append(max_mag)
+        l.append(i)
+    plt.plot(l, normalize(max_fft))
+
+    avg = numpy.average(max_fft)
+    max_fft = [x if x > avg else 0 for x in max_fft]
+    #plt.plot(l, normalize(max_fft))
+
+    start = next(i for i, j in enumerate(max_fft) if j)
+    stop = next(i for i, j in enumerate(max_fft[start:]) if not j) + start
+
+    m = max_fft[start:stop].index(max(max_fft[start:stop])) + start
+    t = m * fft_step
+
+    #plt.plot(t, 1, 'b*')
+    
+    return t
+
+with open(file_name, "rb") as f:
+    data = f.read()
+    if len(data) % 8 != 0: raise Exception("Bad file len")
+
+    struct_fmt = '<' +  len(data)/8 * '2f'
+    struct_unpack = struct.Struct(struct_fmt).unpack_from
+    s = struct_unpack(data)
+    signal = []
+    for i, q in grouped(s, 2):
+        signal.append(complex(i, q))
+    
+    signal_mag = [abs(x) for x in signal]
+    plt.plot(normalize(signal_mag))
+    begin = signal_start(signal)
+    print 'begin', begin
+    
+    # Skip a few samples to have a clean signal
+    signal = signal[begin + skip:]
+    #signal = signal[begin:] 
+    preamble = signal[:fft_length]
+
+    plt.plot([begin+skip, begin+skip], [0, 1], 'r')
+    plt.plot([begin+skip+fft_length, begin+skip+fft_length], [0, 1], 'r')
+    #preamble = preamble * numpy.blackman(len(preamble))
+
+    # Increase size of FFT to inrease resolution
+    preamble = preamble + [complex(0, 0)] * fft_length * 15
+    #fft_length = fft_length * 16
+    preamble = preamble * numpy.blackman(len(preamble))
+    fft_result = numpy.fft.fft(preamble)
+   
+    # Use magnitude of FFT to detect maximum and correct the used bin
+    mag = [abs(x) for x in fft_result]
+    #max_index = list(fft_result.flat).index(numpy.amax(fft_result))
+    max_index = mag.index(max(mag))
+
+    print 'max_index', max_index
+    print 'max_value', fft_result[max_index]
+
+    print 'offset', float(sample_rate)/(fft_length*16)*max_index
+
+    # see http://www.embedded.com/design/configurable-systems/4007643/DSP-Tricks-Spectral-peak-location-algorithm
+    Xmk = fft_result[max_index]
+    Xmkp1 = fft_result[max_index+1]
+    Xmkm1 = fft_result[max_index-1]
+    correction = ((Xmkp1 - Xmkm1) / (2*Xmk - Xmkm1 - Xmkp1)).real
+    offset_freq = float(sample_rate)/float(fft_length * 16)*(max_index - correction)
+
+    print 'correction', correction
+    print 'corrected max', max_index - correction
+    print 'corrected offset', offset_freq
+  
+    single_turn = sample_rate / offset_freq
+
+    shift_signal = [cmath.rect(1, -float(x)/(float(sample_rate)/offset_freq) * 2 * math.pi) for x in range(len(signal))]
+
+    signal = [x*y for x,y in zip(signal, shift_signal)]
+
+    preamble_phase = numpy.average([cmath.phase(x) for x in signal[:fft_length]])
+    signal = [cmath.rect(abs(x), cmath.phase(x) - preamble_phase + math.pi/4) for x in signal]
+    #print numpy.average([cmath.phase(x) for x in signal[:fft_length]])
+    #print numpy.average([x.real for x in signal[:fft_length]])
+    #print numpy.average([x.imag for x in signal[:fft_length]])
+
+    rrc = filters.rrcosfilter(1001, 0.4, 1./25000., 2e6)[1]
+    signal = scipy.signal.convolve(signal, rrc)
+
+    with open(sys.argv[2], 'wb') as out:
+        signal = [item for sample
+                    in signal for item
+                    in [sample.real, sample.imag]]
+        s = "<" + len(signal) * 'f'
+        out.write(struct.Struct(s).pack(*signal))
+       
+    #plt.plot(fft_result)
+    #plt.plot(mag)
+    #plt.plot(preamble)
+    #plt.show()
+
+
---
+++ demod.py
@@ -0,0 +1,197 @@
+#!/usr/bin/python
+# vim: set ts=4 sw=4 tw=0 et pm=:
+import struct
+import sys
+import math
+import numpy
+import os.path
+import cmath
+import filters
+import scipy.signal
+import re
+import sync_search
+
+from itertools import izip
+import matplotlib.pyplot as plt
+
+def grouped(iterable, n):
+    "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
+    return izip(*[iter(iterable)]*n)
+
+def qpsk(angle):
+    if angle<0: angle+=360
+    sym=int(angle)/90
+    off=abs(45-(angle % 90))
+
+    if (off>20): print "Symbol offset >20"
+    return sym
+
+file_name = sys.argv[1]
+schneider=0
+
+if file_name == "-p":
+    file_name=sys.argv[2]
+    schneider=1
+
+sample_rate = 2000000
+symbols_per_second = 25000
+
+samples_per_symbol= float(sample_rate)/float(symbols_per_second)
+
+if int(samples_per_symbol)!=samples_per_symbol: raise Exception("Non-int sps")
+
+samples_per_symbol=int(samples_per_symbol)
+
+skip = 5*samples_per_symbol # beginning might be flakey
+
+def normalize(v):
+    m = max(v)
+
+    return [x/m for x in v]
+
+with open(file_name, "rb") as f:
+    data = f.read()
+    if len(data) % 8 != 0: raise Exception("Bad file len")
+
+    struct_fmt = '<' +  len(data)/8 * '2f'
+    struct_unpack = struct.Struct(struct_fmt).unpack_from
+    s = struct_unpack(data)
+    signal = []
+    for i, q in grouped(s, 2):
+        signal.append(complex(i, q))
+    
+signal_mag = [abs(x) for x in signal]
+
+level=abs(numpy.mean(signal[skip:skip+samples_per_symbol]))
+lmax=abs(numpy.max(signal[skip:skip+samples_per_symbol]))
+print "level: ",level
+
+if schneider==0:
+    # Skip a few samples to have a clean signal
+    print "skip: ",skip
+
+    for i in xrange(skip,len(signal)):
+        lvl= abs(signal[i])/level
+        ang= cmath.phase(signal[i])/math.pi*180
+        
+        if lvl < 0.5:
+            print "First transition is @",i
+            transition=i
+            break
+
+
+    mag = [abs(x) for x in signal[transition:transition+samples_per_symbol]]
+    peak=max(mag)
+    peakidx=transition+mag.index(peak)
+    print "peak is @",peakidx, " (",peak/level,")"
+    start=peakidx-samples_per_symbol
+else:
+    start=sync_search.estimate_sync_word_start(signal, sample_rate, symbols_per_second)
+
+i=start
+bpskbits=12
+symbols=[]
+
+peaks=[complex(-lmax,0)]*len(signal)
+
+mapping= [2,1,-2,-1]
+
+while True:
+    peaks[i]=complex(0,lmax/10.)
+
+    sdiff=2
+
+    try:
+        cur=signal[i].real
+        pre=signal[i-samples_per_symbol].real
+        post=signal[i+samples_per_symbol].real
+        curpre=signal[i-sdiff].real
+        curpost=signal[i+sdiff].real
+
+        if pre<0 and post<0 and cur>0:
+            if curpre>cur and cur>curpost:
+                print "Sampled late"
+                i-=sdiff
+            if curpre<cur and cur<curpost:
+                print "Sampled early"
+                i+=sdiff
+        elif pre>0 and post>0 and cur<0:
+            if curpre>cur and cur>curpost:
+                print "Sampled early"
+                i+=sdiff
+            if curpre<cur and cur<curpost:
+                print "Sampled late"
+                i-=sdiff
+        else:
+            cur=signal[i].imag
+            pre=signal[i-samples_per_symbol].imag
+            post=signal[i+samples_per_symbol].imag
+            post=cur
+            curpre=signal[i-sdiff].imag
+            curpost=signal[i+sdiff].imag
+
+            if pre<0 and post<0 and cur>0:
+                if curpre>cur and cur>curpost:
+                    print "Sampled late"
+                    i-=sdiff
+                if curpre<cur and cur<curpost:
+                    print "Sampled early"
+                    i+=sdiff
+            elif pre>0 and post>0 and cur<0:
+                if curpre>cur and cur>curpost:
+                    print "Sampled early"
+                    i+=sdiff
+                if curpre<cur and cur<curpost:
+                    print "Sampled late"
+                    i-=sdiff
+    except IndexError:
+        print "Last sample"
+
+    lvl= abs(signal[i])/level
+    ang= cmath.phase(signal[i])/math.pi*180
+    symbol=qpsk(ang)
+    symbols=symbols+[symbol]
+
+    print "Symbol @ ",i,": ",symbol," (",ang,",",lvl,")"
+    peaks[i]=complex(+lmax,mapping[symbol]*lmax/5.)
+    i+=samples_per_symbol
+    if i>len(signal) : break
+
+print "Done."
+
+access=""
+for s in symbols[:12]:
+    access+=str(s)
+
+data=""
+for s in symbols[12:]:
+    data+=str(s)
+
+ok="not ok"
+if access=="022220002002": ok="OK"
+
+print "File:",file_name," access: ",access," ",ok," data:",data
+
+# Create r / phi file
+#filename= re.sub('\.raw','.rphi',sys.argv[1])
+#if filename == sys.argv[1]: raise Exception("filename replacement error")
+#
+#with open(filename, 'wb') as out:
+#    signal = [item for sample
+#        in signal for item
+#        in [abs(sample), cmath.phase(sample)]]
+#    s = "<" + len(signal) * 'f'
+#    out.write(struct.Struct(s).pack(*signal))
+
+
+filename= re.sub('\.raw','.peaks',file_name)
+if file_name == filename: raise Exception("filename replacement error")
+
+signal=peaks
+with open(filename, 'wb') as out:
+    signal = [item for sample
+        in signal for item
+        in [sample.real, sample.imag]]
+    s = "<" + len(signal) * 'f'
+    out.write(struct.Struct(s).pack(*signal))
+
