39e2004bfba01a7c8698aa09265ff1156f26f9a7
Stefan `Sec` Zehl <sec@42.org>
2015-01-25 17:00:14 +0100

    Add option to show decoding errors

--- rx-stats.py
+++ rx-stats.py
@@ -10,14 +10,16 @@ import getopt
 
 import bitutils
 
-options, remainder = getopt.getopt(sys.argv[1:], 's:l:o', [
+options, remainder = getopt.getopt(sys.argv[1:], 's:l:eo', [
                                                          'span=',
                                                          'minimum_length=',
+                                                         'errors',
                                                          'lead_out_required'
                                                          ])
 span = 3600
 minimum_length = 0
 lead_out_required = False
+show_errors = False
 
 for opt, arg in options:
     if opt in ('-s', '--span'):
@@ -26,13 +28,15 @@ for opt, arg in options:
         minimum_length = int(arg)
     elif opt in ('-o', '--lead_out_required'):
         lead_out_required = True
+    elif opt in ('-e', '--errors'):
+        show_errors = True
     else:
         print opt
         raise Exception("unknown argument?")
 
 messages = bitutils.read_file(remainder)
 
-timestamps = [s['timestamp'] for s in messages if s['length'] > minimum_length and (not lead_out_required or s['lead_out'])]
+timestamps = [s['timestamp'] for s in messages if s['length'] > minimum_length and (not lead_out_required or s['lead_out']) and (s['error'] == show_errors)]
 
 t0 = min(timestamps)
 t = max(timestamps)
@@ -45,6 +49,8 @@ if lead_out_required:
     title += ', lead out needs to be present'
 else:
     title += ', lead out does not need to be present'
+if show_errors:
+    title += " and having decoding errors"
 
 plt.title(title)
 plt.hist(timestamps, bins)
