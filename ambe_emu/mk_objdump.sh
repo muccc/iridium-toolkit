bfae69f0cb08ac95f16066ed3c1615d5758c2883
Stefan `Sec` Zehl <sec@42.org>
2016-02-21 22:59:39 +0100

    ambe: shallow clone binutils to save space

--- ambe_emu/mk_objdump.sh
+++ ambe_emu/mk_objdump.sh
@@ -1,4 +1,4 @@
-[ ! -d binutils-gdb ] && git clone git://sourceware.org/git/binutils-gdb.git
+[ ! -d binutils-gdb ] && git clone --depth=1 git://sourceware.org/git/binutils-gdb.git
 cd binutils-gdb
 ./configure --target=c54x
 for a in bfd opcodes libiberty zlib binutils ; do
