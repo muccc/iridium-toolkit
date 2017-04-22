2dae0690a5313a4ab37e258dbe0255b960f3f0da
Stefan `Sec` Zehl <sec@42.org>
2017-04-22 19:36:46 +0200

    rewrite reassembler for streaming

--- reassembler.py
+++ reassembler.py
@@ -6,59 +6,60 @@ import fileinput
 import getopt
 import datetime
 import re
+import struct
+import math
+import os
 
 verbose = False
-ifmt= "line"
-ofmt= "undef"
+ifile= None
+ofile= None
+mode= "undef"
 
-options, remainder = getopt.getopt(sys.argv[1:], 'vi:o:', [
+options, remainder = getopt.getopt(sys.argv[1:], 'vi:o:m:', [
                                                          'verbose',
                                                          'input=',
                                                          'output=',
+                                                         'mode=',
                                                          ])
 
 for opt, arg in options:
     if opt in ('-v', '--verbose'):
         verbose = True
     elif opt in ('-i', '--input'):
-        ifmt=arg
+        ifile=arg
     elif opt in ('-o', '--output'):
-        ofmt=arg
+        ofile=arg
+    elif opt in ('-m', '--mode'):
+        mode=arg
     else:
         raise Exception("unknown argument?")
 
-class Message(object):
-    def __init__(self,line):
-            self.typ,self.name,self.time,self.frequency,self.confidence,self.level,self.symbols,self.uldl,self.data=line.split(None,8)
-    def upgrade(self):
-#            return IridiumMessage(self).upgrade()
-        return self
-    def _pretty_header(self):
-        return "%s %s %09d %010d %3d%% %.3f"%(self.typ,self.filename,self.time,self.frequency,self.confidence,self.level)
-    def _pretty_trailer(self):
-        return ""
-    def pretty(self):
-        str= "RAW: "+self._pretty_header()
-        bs=self.bitstream_raw
-        str+=self._pretty_trailer()
-        return str
-
-selected=[]
-
-def do_input(type):
-    if ifmt=="line":
-        for line in fileinput.input(remainder):
-            qqq=re.compile('Warning:')
-            if qqq.match(line):
-                print "Skip: ",line
-                continue
-#            try:
-            perline(Message(line.strip()).upgrade())
-#            except ValueError:
-#                print >> sys.stderr, "Couldn't parse line",line
+basename=None
+if ifile == None:
+    if not remainder:
+        basename="stdin"
+        ifile = "/dev/stdin"
     else:
-        print "Unknown input mode."
-        exit(1)
+        ifile = remainder[0]
+
+if not basename:
+    basename=re.sub('\.[^.]*$','',ifile)
+#    basename=os.path.basename(re.sub('\.[^.]*$','',ifile))
+
+if ofile == None:
+    ofile="%s.%s" % (basename, mode)
+    outfile=sys.stdout
+elif ofile == "" or ofile == "=":
+    ofile="%s.%s" % (basename, mode)
+    outfile=open(ofile,"w")
+else:
+    basename=re.sub('\.[^.]*$','',ofile)
+    outfile=open(ofile,"w")
+
+if verbose:
+    print "ifile",ifile
+    print "ofile",ofile
+    print "basen",basename
 
 def fixtime(n,t):
     try:
@@ -67,12 +68,42 @@ def fixtime(n,t):
     except:
         return int(t)/1000
 
-def perline(q):
-    if False:
+class MyObject(object):
+    pass
+
+class Reassemble(object):
+    def __init__(self):
+        raise Exception("undef")
+    stat_line=0
+    stat_filter=0
+    def run(self,producer):
+        for line in producer:
+            res=self.filter(line)
+            if res != None:
+                self.stat_filter+=1
+                zz=self.process(res)
+                if zz != None:
+                    for mo in zz:
+                        self.consume(mo)
+        self.end()
+    def filter(self,line):
+        self.stat_line+=1
+        q=MyObject()
+        q.typ,q.name,q.time,q.frequency,q.confidence,q.level,q.symbols,q.uldl,q.data=line.split(None,8)
+        q.frequency=int(q.frequency)
+        q.time=int(q.time)
+        q.level=float(q.level)
+        return q
+    def end(self):
+        print "Kept %d/%d (%3.1f%%) lines"%(self.stat_filter,self.stat_line,100.0*self.stat_filter/self.stat_line)
+
+class ReassembleIRA(Reassemble):
+    def __init__(self):
         pass
-    elif ofmt == "page":
+    def filter(self,line):
+        q=super(ReassembleIRA,self).filter(line)
         if q.typ=="IRA:":
-            p=re.compile('.*sat:(\d+) beam:(\d+) pos=\((.[0-9.]+)/(.[0-9.]+)\) alt=([-0-9]+) .* bch:\d+ (.*)')
+            p=re.compile('.*sat:(\d+) beam:(\d+) pos=\((.[0-9.]+)/(.[0-9.]+)\) alt=([-0-9]+) .* bc_sb:\d+ (.*)')
             m=p.match(q.data)
             if(not m):
                 print >> sys.stderr, "Couldn't parse IRA: ",q.data
@@ -82,13 +113,20 @@ def perline(q):
                 q.posx= m.group(3)
                 q.posy= m.group(4)
                 q.alt=  int(m.group(5))
-#                print "%d %d:"%(q.sat,q.beam)
                 p=re.compile('PAGE\(tmsi:([0-9a-f]+) msc_id:([0-9]+)\)')
-                m=p.findall(m.group(6))
-                for x in m:
-                    print "%02d %02d %s %s %03d : %s %s"%(q.sat,q.beam,q.posx,q.posy,q.alt,x[0],x[1])
+                q.pages=p.findall(m.group(6))
+                return q
+    def process(self,q):
+        for x in q.pages:
+            return ["%02d %02d %s %s %03d : %s %s"%(q.sat,q.beam,q.posx,q.posy,q.alt,x[0],x[1])]
+    def consume(self,q):
+        print >> outfile, q
 
-    elif ofmt == "msg":
+class ReassembleMSG(Reassemble):
+    def __init__(self):
+        pass
+    def filter(self,line):
+        q=super(ReassembleMSG,self).filter(line)
         if q.typ == "MSG:":
             #ric:0098049 fmt:05 seq:43 1010010000 1/1 oNEZCOuxvM3PuiQHujzQYd5n0Q8ra0wfMG2WnnhoxAnunT9xzIBSkXyvNP[3]     +11111
             p=re.compile('.* ric:(\d+) fmt:(\d+) seq:(\d+) [01]+ (\d)/(\d) csum:([0-9a-f][0-9a-f]) msg:([0-9a-f]+)\.([01]*) ')
@@ -109,10 +147,14 @@ def perline(q):
 
                 q.msg_msgdata = ''.join(["{0:08b}".format(int(q.msg_hex[i:i+2], 16)) for i in range(0, len(q.msg_hex), 2)])
                 q.msg_msgdata+=q.msg_brest
+
+                # convert to 7bit thingies 
                 m=re.compile('(\d{7})').findall(q.msg_msgdata)
                 q.msg_ascii=""
+                q.msg=[]
                 for (group) in m:
                     character = int(group, 2)
+                    q.msg.append(character)
                     if(character<32 or character==127):
                         q.msg_ascii+="[%d]"%character
                     else:
@@ -121,57 +163,63 @@ def perline(q):
                     q.msg_rest=q.msg_msgdata[-(len(q.msg_msgdata)%7):]
                 else:
                     q.msg_rest=""
-
-                selected.append(q)
-    else:
-        print "Unknown output mode."
-        exit(1)
-
-do_input(input)
-
-if ofmt == "msg":
+                return q
     buf={}
     ricseq={}
     wrapmargin=10
-    for m in selected:
+    def process(self,m):
         # msg_seq wraps around after 61, detect it, and fix it.
-        if m.msg_ric in ricseq:
-            if (m.msg_seq + wrapmargin) < ricseq[m.msg_ric][1]: # seq wrapped around
-                ricseq[m.msg_ric][0]+=62
-            if (m.msg_seq + wrapmargin - 62) > ricseq[m.msg_ric][1]: # "wrapped back" (out-of-order old message)
-                ricseq[m.msg_ric][0]-=62
+        if m.msg_ric in self.ricseq:
+            if (m.msg_seq + self.wrapmargin) < self.ricseq[m.msg_ric][1]: # seq wrapped around
+                self.ricseq[m.msg_ric][0]+=62
+            if (m.msg_seq + self.wrapmargin - 62) > self.ricseq[m.msg_ric][1]: # "wrapped back" (out-of-order old message)
+                self.ricseq[m.msg_ric][0]-=62
         else:
-            ricseq[m.msg_ric]=[0,0]
-        ricseq[m.msg_ric][1]=m.msg_seq
-        id="%07d %04d"%(m.msg_ric,(m.msg_seq+ricseq[m.msg_ric][0]))
+            self.ricseq[m.msg_ric]=[0,0]
+        self.ricseq[m.msg_ric][1]=m.msg_seq
+        id="%07d %04d"%(m.msg_ric,(m.msg_seq+self.ricseq[m.msg_ric][0]))
         ts=m.time
-        if id in buf:
-            if buf[id].msg_checksum != m.msg_checksum:
-                print "Whoa! Checksum changed? Message %s (1: @%d checksum %d/2: @%d checksum %d)"%(id,buf[id].time,buf[id].msg_checksum,m.time,m.msg_checksum)
+        if id in self.buf:
+            if self.buf[id].msg_checksum != m.msg_checksum:
+                print "Whoa! Checksum changed? Message %s (1: @%d checksum %d/2: @%d checksum %d)"%(id,self.buf[id].time,self.buf[id].msg_checksum,m.time,m.msg_checksum)
                 # "Wrap around" to not miss the changed packet.
-                ricseq[m.msg_ric][0]+=62
-                id="%07d %04d"%(m.msg_ric,(m.msg_seq+ricseq[m.msg_ric][0]))
+                self.ricseq[m.msg_ric][0]+=62
+                id="%07d %04d"%(m.msg_ric,(m.msg_seq+self.ricseq[m.msg_ric][0]))
                 m.msgs=['[MISSING]']*3
-                buf[id]=m
+                self.buf[id]=m
         else:
             m.msgs=['[MISSING]']*3
-            buf[id]=m
-        buf[id].msgs[m.msg_ctr]=m.msg_ascii
+            self.buf[id]=m
+        self.buf[id].msgs[m.msg_ctr]=m.msg_ascii
 
-    def messagechecksum(msg):
+    def messagechecksum(self,msg):
         csum=0
         for x in msg:
             csum=(csum+ord(x))%128
         return (~csum)%128
 
-    for b in sorted(buf, key=lambda x: buf[x].time):
-        msg="".join(buf[b].msgs[:1+buf[b].msg_ctr_max])
-        msg=re.sub("(\[3\])+$","",msg) # XXX: should be done differently
-        cmsg=re.sub("\[10\]","\n",msg) # XXX: should be done differently
-#        csum=""
-        csum=messagechecksum(cmsg)
-        str="Message %s @%s (len:%d)"%(b,datetime.datetime.fromtimestamp(buf[b].time).strftime("%Y-%m-%dT%H:%M:%S"),buf[b].msg_ctr_max)
-        str+= " %3d"%buf[b].msg_checksum
-        str+= (" fail"," OK  ")[buf[b].msg_checksum == csum]
-        str+= ": %s"%(msg)
-        print str
+    def consume(self,q):
+        print "consume()"
+        pass
+
+    def end(self): # XXX should be rewritten to consume
+        for b in sorted(self.buf, key=lambda x: self.buf[x].time):
+            msg="".join(self.buf[b].msgs[:1+self.buf[b].msg_ctr_max])
+            msg=re.sub("(\[3\])+$","",msg) # XXX: should be done differently
+            cmsg=re.sub("\[10\]","\n",msg) # XXX: should be done differently
+            csum=self.messagechecksum(cmsg)
+            str="Message %s @%s (len:%d)"%(b,datetime.datetime.fromtimestamp(self.buf[b].time).strftime("%Y-%m-%dT%H:%M:%S"),self.buf[b].msg_ctr_max)
+            str+= " %3d"%self.buf[b].msg_checksum
+            str+= (" fail"," OK  ")[self.buf[b].msg_checksum == csum]
+            str+= ": %s"%(msg)
+            print >> outfile, str
+
+zx=None
+if False:
+    pass
+elif mode == "page":
+    zx=ReassembleIRA()
+elif mode == "msg":
+    zx=ReassembleMSG()
+
+zx.run(fileinput.input(ifile))
