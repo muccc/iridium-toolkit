63921befba5a82465ea1871083c6ee294fab7d12
Guy Taylor <thebigguy.co.uk@gmail.com>
2018-05-10 16:09:46 +0100

    Update stats-voc.py to more modern python
    Start to modernise some of the python scripts. This changes the order of
    some of the comand line args.

--- bits_to_dfs.py
+++ bits_to_dfs.py
@@ -3,6 +3,7 @@
 # vim: set ts=4 sw=4 tw=0 et pm=:
 import fileinput
 import sys
+import argparse
 
 """
 VOC: i-1443338945.6543-t1 033399141 1625872817  81% 0.027 179 L:no LCW(0,001111,100000000000000000000 E1) 01111001000100010010010011011011011001111    011000010000100001110101111011110010010111011001010001011101010001100000000110010100000110111110010101110101001111010100111001000110100110001110110    1010101010010010001000001110011000001001001010011110011100110100111110001101110010110101010110011101011100011101011000000000 descr_extra:
@@ -37,23 +38,39 @@ def chunks(l, n):
     for i in xrange(0, len(l), n):
         yield l[i:i+n]
 
-infile = sys.argv[1]
-outfile = open(sys.argv[2], 'wb')
 
-data = ''
+def bits_to_dfs(lines, output):
+    data = ''
+    for line in lines:
+        line = line.split()
+        if line[0] == 'VOC:':
+            if int(line[6]) < 179:
+                continue
+            data = line[10]
+            if (data[0] == "["):
+                for pos in xrange(1,len(data),3):
+                    byte=int(data[pos:pos+2],16)
+                    byte=int('{:08b}'.format(byte)[::-1], 2)
+                    output.write(chr(byte))
+            else:
+                for bits in chunks(data, 8):
+                    byte = int(bits[::-1],2)
+                    output.write(chr(byte))
 
-for line in fileinput.input(infile):
-    line = line.split()
-    if line[0] == 'VOC:':
-        if int(line[6]) < 179:
-            continue
-        data = line[10]
-        if (data[0] == "["):
-            for pos in xrange(1,len(data),3):
-                byte=int(data[pos:pos+2],16)
-                byte=int('{:08b}'.format(byte)[::-1], 2)
-                outfile.write(chr(byte))
-        else:
-            for bits in chunks(data, 8):
-                byte = int(bits[::-1],2)
-                outfile.write(chr(byte))
+def main():
+    parser = argparse.ArgumentParser(description='Convert iridium-parser.py VOC output to DFS')
+    parser.add_argument('output', metavar='OUTPUT', help='Output file for DFS content. If - stdout is used ')
+    parser.add_argument('input', metavar='FILE', nargs='*', help='Files to read, if empty or -, stdin is used')
+    args = parser.parse_args()
+
+    output_file = sys.stdout if args.output == '-' else open(args.output, 'w')
+    input_files = args.input if len(args.input) > 0 else ['-']
+
+    bits_to_dfs(fileinput.input(files=input_files), output_file)
+
+    if output_file is not sys.stdout:
+        output_file.close()
+
+
+if __name__ == '__main__':
+    main()
--- stats-voc.py
+++ stats-voc.py
@@ -5,85 +5,117 @@
 import sys
 import matplotlib.pyplot as plt
 import os
+import subprocess
+import fileinput
+import logging
+import tempfile
+from bits_to_dfs import bits_to_dfs
 
-def filter_voc(t_start = None, t_stop = None, f_min = None, f_max = None):
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def filter_voc(lines, t_start = None, t_stop = None, f_min = None, f_max = None):
     tsl = []
     fl = []
-    lines = []
-    f = open(sys.argv[1])
+    filtered_lines = []
 
-    for line in f:
+    for line in lines:
+        line_split = line.split()
+        lcw = line[8]
+        #ts_base = int(line[1].split('-')[1].split('.')[0])
+        ts_base = 0
+        ts = ts_base + int(line_split[2])/1000.
+        f = int(line_split[3])/1000.
+        if ((not t_start or t_start <= ts) and
+                (not t_stop or ts <= t_stop) and
+                (not f_min or f_min <= f) and
+                (not f_max or f <= f_max)):
+            tsl.append(ts)
+            fl.append(f)
+            filtered_lines.append(line)
+    return tsl, fl, filtered_lines
+
+
+class OnClickHandler(object):
+    def __init__(self, lines):
+        self.lines = lines
+        self.t_start = None
+        self.t_stop = None
+        self.f_min = None
+        self.f_max = None
+
+    def onclick(self, event):
+        logger.info('button=%d, x=%d, y=%d, xdata=%f, ydata=%f',
+            event.button, event.x, event.y, event.xdata, event.ydata)
+
+        if event.button == 1:
+            self.t_start = event.xdata
+            self.f_min = event.ydata
+            self.t_stop = None
+            self.f_max = None
+        if event.button == 3:
+            self.t_stop = event.xdata
+            self.f_max = event.ydata
+        
+        if self.t_start and self.t_stop:
+            self.cut_convert_play(self.t_start, self.t_stop, self.f_min, self.f_max)
+
+    def cut_convert_play(self, t_start, t_stop, f_min, f_max):
+        logger.info('Starting to play...')
+        if t_start and t_stop:
+            if t_start > t_stop:
+                tmp = t_stop
+                t_stop = t_start
+                t_start = tmp
+            if f_min > f_max:
+                tmp = f_max
+                f_max = f_min
+                f_min = tmp
+
+        _, _, filtered_lines = filter_voc(self.lines, t_start=t_start, t_stop=t_stop, f_min=f_min, f_max=f_max)
+
+        _, dfs_file_path = tempfile.mkstemp(suffix='.dfs')
+        _, wav_file_path = tempfile.mkstemp(suffix='.wav')
+
+        logger.info('Making dfs file %s', dfs_file_path)
+        with open(dfs_file_path, 'w') as dfs_file:
+            bits_to_dfs(filtered_lines, dfs_file)
+
+        logger.info('Making wav file %s', wav_file_path)
+        subprocess.check_call(['ir77_ambe_decode', dfs_file_path, wav_file_path])
+        
+        logger.info('Cleaning up dfs')
+        os.remove(dfs_file_path)
+
+        subprocess.check_call(['aplay', wav_file_path])
+
+        logger.info('Cleaning up wav')
+        os.remove(wav_file_path)
+
+        logger.info('Finished Playing')
+
+
+def read_lines():
+    lines = []
+    for line in fileinput.input():
         line = line.strip()
         if 'VOC: ' in line and not "LCW(0,001111,100000000000000000000" in line:
-            line_split = line.split()
-            lcw = line[8]
-            #ts_base = int(line[1].split('-')[1].split('.')[0])
-            ts_base = 0
-            ts = ts_base + int(line_split[2])/1000.
-            f = int(line_split[3])/1000.
-            if ((not t_start or t_start <= ts) and
-                    (not t_stop or ts <= t_stop) and
-                    (not f_min or f_min <= f) and
-                    (not f_max or f <= f_max)):
-                tsl.append(ts)
-                fl.append(f)
-                lines.append(line)
-    return tsl, fl, lines
-
-
-def cut_convert_play(t_start, t_stop, f_min, f_max):
-    if t_start and t_stop:
-        if t_start > t_stop:
-            tmp = t_stop
-            t_stop = t_start
-            t_start = tmp
-        if f_min > f_max:
-            tmp = f_max
-            f_max = f_min
-            f_min = tmp
-
-    f_out = open('/tmp/voice.bits', 'w')
-    _, _, lines = filter_voc(t_start, t_stop, f_min, f_max)
-    for line in lines:
-        f_out.write(line + "\n")
-    f_out.close()
-    os.system("play-iridium-ambe /tmp/voice.bits")
-
-
-def onclick(event):
-    global t_start, t_stop, f_min, f_max
-    print 'button=%d, x=%d, y=%d, xdata=%f, ydata=%f'%(
-        event.button, event.x, event.y, event.xdata, event.ydata)
-    if event.button == 1:
-        t_start = event.xdata
-        f_min = event.ydata
-        t_stop = None
-        f_max = None
-    if event.button == 3:
-        t_stop = event.xdata
-        f_max = event.ydata
-    
-    if t_start and t_stop:
-        cut_convert_play(t_start, t_stop, f_min, f_max)
+            lines.append(line)
+    return lines
 
 def main():
-    tsl, fl, _ = filter_voc()
-
-    print len(tsl)
+    lines = read_lines()
+    logger.info('Read %d VOC lines from input', len(lines))
+    tsl, fl, _ = filter_voc(lines)
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(x = tsl, y = fl)
 
-    t_start = None
-    t_stop = None
-    f_min = None
-    f_max = None
+    on_click_handler = OnClickHandler(lines)
+    cid = fig.canvas.mpl_connect('button_press_event', on_click_handler.onclick)
 
-
-    cid = fig.canvas.mpl_connect('button_press_event', onclick)
-
-    plt.title("Click once left and once rigth to define an area. The script will try to play iridium using the play-iridium-ambe shell script.")
+    plt.title('Click once left and once right to define an area. The script will try to play iridium using ir77_ambe_decode and aplay.')
     plt.show()
 
 
