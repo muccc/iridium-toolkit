ef6dcdd849828fa6ae0ec171d16fa572d6b53150
Stefan `Sec` Zehl <sec@42.org>
2020-09-11 19:36:40 +0200

    [parser] clarify & cut erasure from rs_fix return

--- rs.py
+++ rs.py
@@ -3,19 +3,20 @@
 
 import reedsolo
 
+# IIQ/LCW3:  Message: 31B, checksum: 8B, erasure: 8B - RS(47,31) [total: 312b]
+
 generator=2
-fcr=0
-nsym=16
+fcr=0   # first consecutive root
+nsym=8  # number of ecc symbols (n-k)
+elen=8  # erasure length (how many bytes erased at end)
 c_exp=8 # bits per symbol
 prim=0x11d
 reedsolo.init_tables(prim=prim,generator=generator,c_exp=c_exp)
 
-# erasure length
-elen=8
 
 def rs_check(data):
-	mlen=len(data)+elen-nsym
-	msg=reedsolo.rs_encode_msg(data[:mlen],nsym,fcr=fcr)
+	mlen=len(data)-nsym
+	msg=reedsolo.rs_encode_msg(data[:mlen],nsym+elen,fcr=fcr)
 	return bytearray(data[mlen:])==msg[mlen:len(data)]
 
 def rs_fix(data):
@@ -23,9 +24,9 @@ def rs_fix(data):
 	data=data+([0]*elen)
 	r=range(len(data)-elen,len(data))
 	try:
-		(cmsg,crs)=reedsolo.rs_correct_msg(data,nsym,fcr,generator,r)
+		(cmsg,crs)=reedsolo.rs_correct_msg(data,nsym+elen,fcr,generator,erase_pos=r)
 	except reedsolo.ReedSolomonError:
 		return (False,None,None)
 	except ZeroDivisionError:
 		return (False,None,None)
-	return (True,cmsg,crs)
+	return (True,cmsg,crs[:nsym])
--- rs6.py
+++ rs6.py
@@ -3,18 +3,20 @@
 
 import reedsolo6
 
+# VO6/LCW3: Message: 42*6b=31.5B, checksum: 10*6b=7.5B - RS_6(52,10) [total: 312b]
+
 generator=2
 fcr=54
-nsym=10
-c_exp=6
+nsym=10 # number of ecc symbols (n-k)
+elen=0  # erasure length (how many bytes erased at end)
+c_exp=6 # bits per symbol
 prim=0x43
 reedsolo6.init_tables(prim=prim,generator=generator,c_exp=c_exp)
 
-elen=0
 
 def rs_check(data):
-	mlen=len(data)+elen-nsym
-	msg=reedsolo6.rs_encode_msg(data[:mlen],nsym,fcr=fcr)
+	mlen=len(data)-nsym
+	msg=reedsolo6.rs_encode_msg(data[:mlen],nsym+elen,fcr=fcr)
 	return bytearray(data[mlen:])==msg[mlen:len(data)]
 
 def rs_fix(data):
@@ -22,9 +24,9 @@ def rs_fix(data):
 	data=data+([0]*elen)
 	r=range(len(data)-elen,len(data))
 	try:
-		(cmsg,crs)=reedsolo6.rs_correct_msg(data,nsym,fcr,generator,r)
+		(cmsg,crs)=reedsolo6.rs_correct_msg(data,nsym+elen,fcr,generator,r)
 	except reedsolo6.ReedSolomonError:
 		return (False,None,None)
 	except ZeroDivisionError:
 		return (False,None,None)
-	return (True,cmsg,crs)
+	return (True,cmsg,crs[:nsym])
