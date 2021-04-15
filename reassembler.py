muccc/iridium-toolkit

commit 392d4b203d56f4d66c8659c3d1022b0aa2c7a603

[reassembler] proper error message on unknown mode

reassembler.py
@@ -918,6 +918,9 @@ elif mode == "stats":
     zx=StatsPKT()
 elif mode == "ppm":
     zx=ReassemblePPM()
+else:
+    print >>sys.stderr, "Unknown mode selected"
+    sys.exit(1)
 
 for x in args.keys():
     if x not in validargs:

commit 5a348aa8cca57f1f5229f87bc9f77baeeb2929e4

[reassembler] fix logic bug introduced in 392d4b2

reassembler.py
@@ -893,13 +893,12 @@ class ReassembleMSG(Reassemble):
 
 validargs=()
 zx=None
-if False:
-    pass
+
 if mode == "ida":
     zx=ReassembleIDA()
-if mode == "idapp":
+elif mode == "idapp":
     zx=ReassembleIDAPP()
-if mode == "gsmtap":
+elif mode == "gsmtap":
     zx=ReassembleIDALAP()
 elif mode == "lap":
     validargs=('all')
@@ -907,7 +906,7 @@ elif mode == "lap":
         ofile="%s.%s" % (basename, "pcap")
         outfile=open(ofile,"w")
     zx=ReassembleIDALAPPCAP()
-if mode == "sbd":
+elif mode == "sbd":
     zx=ReassembleIDASBD()
 elif mode == "page":
     zx=ReassembleIRA()