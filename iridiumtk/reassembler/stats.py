f9add3d283577cfbb4ac769b1c0f698b04244b75
Stefan `Sec` Zehl <sec@42.org>
2021-11-10 14:06:59 +0100

    [reassembler] add stats-pkt and stats-ecc modes

---
+++ iridiumtk/reassembler/stats.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# vim: set ts=4 sw=4 tw=0 et pm=:
+
+import re
+import datetime
+
+from .base import *
+from ..config import config, outfile, state
+
+class StatsPKT(Reassemble):
+    stats={}
+
+    def __init__(self):
+        for k in ['UL', 'DL', 'perfect']:
+            self.stats[k]={}
+            for x in ['IBC', 'IDA', 'IIP', 'IIQ', 'IIR', 'IIU', 'IMS', 'IRA', 'IRI', 'ISY', 'ITL', 'IU3', 'I36', 'I38', 'MSG', 'VDA', 'VO6', 'VOC', 'VOD', 'MS3', 'VOZ']:
+                self.stats[k][x]=0
+        del self.stats['UL']['ITL']
+        del self.stats['UL']['IMS']
+        del self.stats['UL']['MSG']
+        del self.stats['UL']['MS3']
+        pass
+
+    def filter(self,line):
+        q=super().filter(line)
+
+        if q==None: return None
+        if q.typ[3]!=":": return None
+        if q.typ=="RAW:": return None
+        if q.typ=="IME:": return None
+        return q
+
+    def process(self,q):
+        typ=q.typ[0:3]
+        self.stats[q.uldl][typ]+=1
+        if q.name.endswith("-e000"):
+            self.stats["perfect"][typ]+=1
+        return None
+
+    def end(self):
+        total=0
+        perfect=0
+        uplink=0
+        downlink=0
+        for t in self.stats["DL"]:
+            tsum=self.stats["DL"][t]
+            if t in self.stats["UL"]:
+                tsum+=self.stats["UL"][t]
+            total+=tsum
+            print("%7d good.%s"%(tsum,t))
+            perfect+=self.stats["perfect"][t]
+            print("%7d perfect.%s"%(self.stats["perfect"][t],t))
+            if t in self.stats["UL"]:
+                uplink+=self.stats["UL"][t]
+                print("%7d uplink.%s"%(self.stats["UL"][t],t))
+            downlink+=self.stats["DL"][t]
+        print("%7d total.parsed"%(total))
+        print("%7d total.perfect"%(perfect))
+        print("%7d total.downlink"%(downlink))
+        print("%7d total.uplink"%(uplink))
+
+class StatsECC(Reassemble):
+    stats={}
+
+    def __init__(self):
+        self.r_uw=re.compile('-e[1-9][0-9][0-9]$')
+        self.r_lcw=re.compile('-e0[1-9][0-9]$')
+        self.r_fix=re.compile('-e00[1-9]$')
+        self.r_ok=re.compile('-e000$')
+        self.uw=0
+        self.lcw=0
+        self.fix=0
+        pass
+
+    def filter(self,line):
+        q=super().filter(line)
+
+        if q==None: return None
+        if q.typ[3]!=":": return None
+        if q.typ=="RAW:": return None
+        if q.typ=="IME:": return None
+        return q
+
+    def process(self,q):
+        if (self.r_uw.search(q.name)):
+            self.uw+=1
+        elif (self.r_lcw.search(q.name)):
+            self.lcw+=1
+        elif (self.r_fix.search(q.name)):
+            self.fix+=1
+        elif (self.r_ok.search(q.name)):
+            pass
+        else:
+            raise Exception("No ECC info found: "+q.name)
+        return None
+
+    def end(self):
+        print("%7d ecc.uniq_word"%(self.uw))
+        print("%7d ecc.link_control_word"%(self.lcw))
+        print("%7d ecc.content"%(self.fix))
+#        print "%7d total.unparsed"%()
+
+modes=[
+["stats-pkt", StatsPKT,          ],
+["stats-ecc", StatsECC,          ],
+]
