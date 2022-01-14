b345d47aff39dd0261be6e502eb68ee13c022320
Stefan `Sec` Zehl <sec@42.org>
2022-01-14 17:07:14 +0100

    [reassembler] itlmap: mode to map satellite numbers to logical positions

--- reassembler.py
+++ reassembler.py
@@ -114,12 +114,19 @@ Z=Zulu()
 pwarn=False
 
 class MyObject(object):
-    def enrich(self):
+    def enrich(self, channelize=False):
         if "|" in self.frequency:
             chan, off=self.frequency.split('|')
             self.frequency=base_freq+channel_width*int(chan)+int(off)
         else:
             self.frequency=int(self.frequency)
+
+        if channelize:
+            fbase=self.frequency-base_freq
+            self.freq_chan=int(fbase/channel_width)
+            foff =fbase%channel_width
+            self.freq_off=foff-(channel_width/2)
+            self.freq_print="%3d|%+06d"%(self.freq_chan,self.freq_off)
 
         if len(self.name) > 3 and self.name[1]=='-':
             self.ftype=self.name[0]
@@ -616,6 +623,104 @@ class ReassemblePPM(Reassemble):
         print("rec.tmin %f"%(self.tmin))
         print("rec.tmax %f"%(self.tmax))
         print("rec.ppm %.3f"%(delta/alltime*1000000))
+
+class InfoITLSatMap(Reassemble):
+    def __init__(self):
+        self.itl=None
+        self.ira=None
+        self.store= {}
+        pass
+    def filter(self,line):
+        q=super().filter(line)
+        if q==None: return None
+        if q.typ!="IRA:" and q.typ!="ITL:": return None
+        if q.typ=="IRA:":
+            p=re.compile(r'sat:(\d+) beam:(\d+)')
+            m=p.search(q.data)
+            if(not m):
+                print("Couldn't parse IRA: ",q.data, end=' ', file=sys.stderr)
+                return None
+            else:
+                q.enrich(True)
+                q.sat=  int(m.group(1))
+                q.beam= int(m.group(2))
+                self.ira=q
+        elif q.typ=="ITL:":
+            p=re.compile(r'V[12] OK P(\d+) (?:---|S(\d+)) ')
+            m=p.search(q.data)
+            if(not m):
+                print("Couldn't parse ITL: ",q.data, end=' ', file=sys.stderr)
+                return None
+            elif m.group(2) is None:
+                return None
+            else:
+                q.enrich(True)
+                q.plane= int(m.group(1))
+                q.satno= int(m.group(2))
+                self.itl=q
+
+        if self.itl is None or self.ira is None:
+            return None
+
+        if abs(self.itl.mstime - self.ira.mstime)<0.01:
+            df=self.ira.frequency-self.itl.frequency
+            if verbose:
+                print("Match: delta_f=%d"%df)
+                print("- IRA",self.ira.mstime,self.ira.frequency,self.ira.freq_print,self.ira.sat)
+                print("- ITL",self.itl.mstime,self.itl.frequency,self.itl.freq_print,"P%dS%02d"%(self.itl.plane,self.itl.satno))
+            if abs(df-(4*channel_width))<300:
+                q=MyObject()
+                q.sat=self.ira.sat
+                q.plane=self.itl.plane
+                q.itlsatno=self.itl.satno
+                return q
+
+        return None
+
+    def process(self,q):
+        ps="P%dS%02d"%(q.plane,q.itlsatno)
+        if ps not in self.store:
+            self.store[ps]={}
+        if q.sat not in self.store[ps]:
+            self.store[ps][q.sat]=0
+        self.store[ps][q.sat]+=1
+
+
+    def consume(self,q):
+        raise Exception("unreachable")
+
+    def end(self):
+        print("Iridium satellite ordering (using iridium-internal identifiers)")
+        print("")
+        print("        ",end=' ')
+        for x in range(1,12):
+            print("%3d"%x,end=' ')
+        print("")
+
+        for plane in range(1,7):
+            print("Plane %d:"%plane,end=' ')
+            for idx in range(1,12):
+                i="P%dS%02d"%(plane,idx)
+
+                _sum=0
+                _max=0
+                maxname=None
+                try:
+                    for sat,count in self.store[i].items():
+                        _sum+=count
+                        if count > _max:
+                            maxname=sat
+                            _max=count
+                    conf=_max*100/_sum
+                except KeyError:
+                    maxname="?"
+                    conf=100
+
+                if conf<98:
+                    print("%3s(%2d%%)"%(maxname,conf),end=' ')
+                else:
+                    print("%3s"%(maxname),end=' ')
+            print("")
 
 class ReassembleIDA(Reassemble):
     def __init__(self):
@@ -1921,6 +2026,8 @@ elif mode == "live-map":
 elif mode == "ppm":
     validargs=('perfect','grafana','tdelta')
     zx=ReassemblePPM()
+elif mode == "itlmap":
+    zx=InfoITLSatMap()
 else:
     print("Unknown mode selected", file=sys.stderr)
     sys.exit(1)
