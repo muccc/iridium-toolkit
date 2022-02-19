5599bb5f4e9a4cc445d93312862a5b7d034716b0
Stefan `Sec` Zehl <sec@42.org>
2022-02-19 15:53:45 +0100

    [reassembler] support for -m live-map

---
+++ html/example.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ -d html ] ; then
+  cd html
+fi
+
+
+
+../reassembler.py -m live-map --stats zmq: &
+reass=$!
+
+killer(){
+    kill $reass
+    exit
+}
+trap killer INT
+
+echo
+echo "open http://localhost:8888/map.html in your browser"
+echo ""
+echo "and make sure 'iridium-parser' is running with -o zmq on this host"
+echo
+
+python -m http.server --bind 127.0.0.1 8888
+
+kill $reass
