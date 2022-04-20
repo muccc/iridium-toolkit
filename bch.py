561ce9996e5e6939a94b8ed2abb34b0d93b39b1a
Stefan `Sec` Zehl <sec@42.org>
2022-04-20 13:46:42 +0200

    [bch] implement error correction via syndromes

--- bch.py
+++ bch.py
@@ -116,7 +116,7 @@ def nrepair1(a,b): # "repair" one bit error by brute force.
             return (1,bnum1str)
     return(-1,b)
 
-def nrepair(a,b): # "repair" two bit errors by brute force.
+def nrepair2(a,b): # "repair" two bit errors by brute force.
     r=ndivide(a,b)
     if(r==0):
         return (0,b)
@@ -138,10 +138,99 @@ def nrepair(a,b): # "repair" two bit errors by brute force.
                 return (2,bnum2str)
     return(-1,b)
 
+def nrepair(poly, b): # "repair" any bit errors by syndromes
+    r=nndivide(poly, int(b,2))
+    if(r==0):
+        return (0,b)
+    if syndromes[poly][r] is None: # uncorrectable
+        return(-1,b)
+
+    ecnt, eloc = syndromes[poly][r]
+    bnum=eloc ^ int(b,2)
+
+    fstr="{0:0%db}"%len(b)
+    return (ecnt,fstr.format(bnum))
+
 def bch_repair1(poly,bits):
     (errs,repaired)=nrepair1(poly,bits)
+    return (errs,repaired[:-poly.bit_length()+1],repaired[-poly.bit_length()+1:])
+
+def bch_repair2(poly,bits):
+    (errs,repaired)=nrepair2(poly,bits)
     return (errs,repaired[:-poly.bit_length()+1],repaired[-poly.bit_length()+1:])
 
 def bch_repair(poly,bits):
     (errs,repaired)=nrepair(poly,bits)
     return (errs,repaired[:-poly.bit_length()+1],repaired[-poly.bit_length()+1:])
+
+def mk_syn(poly, bits, synbits, errors=1, debug=False):
+    assert errors in (1,2,3)
+    syndromes[poly]=[None]*(2**(synbits))
+
+    if debug:
+        print("Creating syndromes for poly=%d with %d bits and max %d bit-errors"%(poly, bits, errors))
+        print("Max syndrome value is: 2^%d = %d"%(synbits, 2**synbits))
+
+    for n1 in range(0,bits):
+        val=(1<<n1)
+        r=nndivide(poly,val)
+        if debug:
+            print(("1 {:0%db} -> {:4d} / {:0%db}"%(bits,synbits)).format(val,r,r))
+        syndromes[poly][r]=(1, val)
+
+    if errors >= 2:
+        for n1 in range(0,bits):
+            for n2 in range(n1+1,bits):
+                val=(1<<n1)|(1<<n2)
+                r=nndivide(poly,val)
+                if debug:
+                    print(("2 {:0%db} -> {:4d} / {:0%db}"%(bits,synbits)).format(val,r,r))
+                if syndromes[poly][r] is None:
+                    syndromes[poly][r]=(2, val)
+                else:
+                    raise AssertionError("Poly(%d) collision on syndrome %d (error locators %s / %s)"%(poly, r, bin(val), bin(syndromes[poly][r][1])))
+
+    if errors == 3:
+        for n1 in range(0,bits):
+            for n2 in range(n1+1,bits):
+                for n3 in range(n2+1,bits):
+                    val=(1<<n1)|(1<<n2)|(1<<n3)
+                    r=nndivide(poly,val)
+                    if debug:
+                        print(("3 {:0%db} -> {:4d} / {:0%db}"%(bits,synbits)).format(val,r,r))
+                    if syndromes[poly][r] is None:
+                        syndromes[poly][r]=(3, val)
+                    else:
+                        raise AssertionError("Poly(%d) collision on syndrome %d (error locators %s / %s)"%(poly, r, bin(val), bin(syndromes[poly][r][1])))
+
+    if debug:
+        elems=sum(x is not None for x in syndromes[poly])
+        elems+=1 # 0 element
+        print("BCH efficiency: %d/%d %02d%%"%(elems,2**synbits,(2*elems/2**synbits)*100))
+
+def print_syn(syndromes, bits=31, synbits=10):
+    for r, v in enumerate(syndromes):
+        if v is None:
+            print(("> {:%ds} -> {:4d} / {:0%db}"%(bits,synbits)).format("",r,r))
+        else:
+            eb, val = v
+            if val == -1:
+                print(("c {:%ds} -> {:4d} / {:0%db}"%(bits,synbits)).format("",r,r))
+            else:
+                print(("%d {:0%db} -> {:4d} / {:0%db}"%(eb, bits,synbits)).format(val,r,r))
+
+
+syndromes={}
+
+def init(debug=False):
+    mk_syn(poly=29,   bits=7,  synbits=4,            debug=debug)
+    mk_syn(poly=465,  bits=14, synbits=8,  errors=2, debug=debug)
+    mk_syn(poly=41,   bits=26, synbits=5,            debug=debug)
+    mk_syn(poly=1897, bits=31, synbits=10, errors=2, debug=debug)
+    mk_syn(poly=1207, bits=31, synbits=10, errors=2, debug=debug)
+    mk_syn(poly=3545, bits=31, synbits=11, errors=2, debug=debug)
+
+if __name__ == "__main__":
+    init(True)
+else:
+    init()
