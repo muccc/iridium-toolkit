f660f037313b10e85b56b2271f126a8593f03f17
Kevin Elliott <kevin@welikeinc.com>
2022-11-08 17:40:42 +0000

    Add support for TCP and UDP for the sender script

--- iridium-acars-to-airframes.py
+++ iridium-acars-to-airframes.py
@@ -2,8 +2,8 @@
 #
 # iridium-acars-to-airframes.py
 #
-# Send the ACARS JSON output from iridium-toolkit to Airframes.io. You may send to additional outputs by
-# specifying the --output option multiple times.
+# Send the ACARS JSON output from iridium-toolkit to Airframes.io. You may send to additional TCP destinations
+# by specifying the --output option multiple times.
 #
 # INSTRUCTIONS:
 #
@@ -28,7 +28,7 @@
 #                           Override station ident
 #     --debug, -d           Enable debug output
 #     --output OUTPUT, -o OUTPUT
-#                           Send output via TCP to additional destination host:port
+#                           Send output via TCP to additional destination transport:host:port (where transport is "tcp" or "udp")
 #
 # For more information about Airframes, see https://app.airframes.io/about or contact kevin@airframes.io.
 #
@@ -46,13 +46,6 @@ debug = True
 sock = None
 sockets = {}
 
-def connect():
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.connect((ingest_host, ingest_port))
-  if debug:
-    print("Connected to Airframes Iridium ACARS ingest (%s:%d)" % (ingest_host, ingest_port))
-  return s
-
 def reconnect():
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.connect((ingest_host, ingest_port))
@@ -69,18 +62,20 @@ def send_message(message):
         print("Sent message to %s" % (k))
     except Exception as e:
       print('Error sending message: %s' % e)
-      sockets[k] = reconnect()
+      sock.connect(sock.getpeername())
+      sockets[k] = sock
+      print("Reconnected to %s:%d" % (sock.getpeername()))
       sockets[k].sendall(message.encode('utf-8'))
 
 
 if __name__ == '__main__':
   args_parser = argparse.ArgumentParser(
     prog = 'iridium-acars-to-airframes.py',
-    description='Feed Iridium ACARS to Airframes.io'
+    description='Feed Iridium ACARS to Airframes.io and additional remote destinations',
   )
   args_parser.add_argument('--station', '-s', help='Override station ident', required=False)
   args_parser.add_argument('--debug', '-d', help='Enable debug output', action='store_true')
-  args_parser.add_argument('--output', '-o', help='Send output via TCP to additional destination host:port', default=['%s:%s' % (airframes_ingest_host, airframes_ingest_port)], action='append')
+  args_parser.add_argument('--output', '-o', help='Send output via TCP to additional destination transport:host:port (where transport is "tcp" or "udp")', default=['tcp:%s:%s' % (airframes_ingest_host, airframes_ingest_port)], action='append')
   args = args_parser.parse_args()
 
   debug = args.debug
@@ -88,13 +83,22 @@ if __name__ == '__main__':
     print("Overriding station ident to %s" % (args.station,))
 
   for output in args.output:
-    host, port = output.split(':')
+    transport, host, port = output.split(':')
     try:
-      sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      sock.connect((host, int(port)))
-      sockets[output] = sock
-      if debug:
-        print("Connected to %s:%s" % (host, port))
+      if transport == 'tcp':
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, int(port)))
+        sockets[output] = sock
+        if debug:
+          print("Connected to %s:%s:%s" % (transport, host, port))
+      elif transport == 'udp':
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.connect((host, int(port)))
+        sockets[output] = sock
+        if debug:
+          print("Connected to %s:%s:%s" % (transport, host, port))
+      else:
+        print("Unknown transport %s" % (transport,))
     except TimeoutError as e:
       print("Error connecting to output (%s:%s): %s" % (host, port, e))
       sys.exit(1)
