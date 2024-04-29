4a67ad3c8b1820b01c7dc04f29ca38840c3696ae
Stefan `Sec` Zehl <sec@42.org>
2024-04-29 15:05:53 +0200

    [reassembler] mtmap: formatting / pep8

--- iridiumtk/reassembler/mtpos.py
+++ iridiumtk/reassembler/mtpos.py
@@ -13,6 +13,7 @@ from .base import *
 from .ida import ReassembleIDA
 from ..config import config, outfile
 
+
 class ReassembleIDAMap(ReassembleIDA):
     """Extract coordinates from access decision messages"""
 
@@ -29,29 +30,27 @@ class ReassembleIDAMap(ReassembleIDA):
         if config.stats:
             from util import curses_eol
             global eol
-            eol=curses_eol()
-
+            eol = curses_eol()
 
     def consume(self, q):
-        (data, time, ul, _, freq)=q
+        (data, time, ul, _, freq) = q
         if len(data) < 2:
             return
 
         m_type = data[:2].hex()
 
-        if m_type !="0605" and m_type != "7605":
+        if m_type != "0605" and m_type != "7605":
             return
 
         if ul:
-            ul="UL"
+            ul = "UL"
         else:
-            ul="DL"
-
+            ul = "DL"
 
         if m_type == "7605":
             if len(data) > 5 and data[2] == 0 and data[3]&0xf0 == 0x40:
                 type = 'sbd'
-                pos=xyz(data[3:], 4)
+                pos = xyz(data[3:], 4)
             elif len(data) > 3 and data[3] == 0x50: # ack only
                 return
             else: # no match
@@ -60,7 +59,7 @@ class ReassembleIDAMap(ReassembleIDA):
             off = (2+1+20+1+3+3+2+2+2)
             if len(data) > off+2 and data[off] == 0x1b:
                 type = 'gsm'
-                pos=xyz(data[off+1:], 0)
+                pos = xyz(data[off+1:], 0)
                 #lac_o=2+1+20
                 #if data[lac_o] == 0x04:
                 #    print("lac=",data[lac_o+1:lac_o+3].hex())
@@ -77,20 +76,19 @@ class ReassembleIDAMap(ReassembleIDA):
             self.last_output = time
             self.mt_pos = [x for x in self.mt_pos if x['ts'] > time - self.exptime]
 
-            ofile=config.output
+            ofile = config.output
             if ofile is None:
-                ofile="mt.json"
-            temp_file_path="%s.tmp"%(ofile)
+                ofile = "mt.json"
+            temp_file_path = "%s.tmp" % (ofile)
             with open(temp_file_path, "w") as f:
                 print(json.dumps({"time": int(time), "interval": self.intvl, "mt_pos": self.mt_pos}, separators=(',', ':')), file=f)
             os.rename(temp_file_path, ofile)
             if config.stats:
-                sts=dt.epoch(int(time))
-                mts=len(self.mt_pos)
-                print("%s: %d MTs"%(sts,mts), end=eol, file=sys.stderr)
+                sts = dt.epoch(int(time))
+                mts = len(self.mt_pos)
+                print("%s: %d MTs" % (sts, mts), end=eol, file=sys.stderr)
 
 
-
-modes=[
-["live-mt-map",        ReassembleIDAMap, ],
+modes = [
+    ["live-mt-map", ReassembleIDAMap, ],
 ]
